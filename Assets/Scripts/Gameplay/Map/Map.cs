5746d946d11595a45ced257aacec39f581666c6e
TomHuizing <tom.huizing@protonmail.com>
2025-05-22 16:02:52 +0200

    Added Size2 and Size3, GeoCoord and GeoRect

--- Assets/Scripts/Gameplay/Map/Map.cs
+++ Assets/Scripts/Gameplay/Map/Map.cs
@@ -1,15 +1,35 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Map : MonoBehaviour
 {
+    [Serializable]
+    public struct GeoRectInspector
+    {
+        public GeoCoordInspector Center;
+        public float Width;
+        public float Height;
+
+        public GeoRect ToGeoRect() => new(Center.ToGeoCoord(), Width, Height);
+    }
+
+    [Serializable]
+    public struct GeoCoordInspector
+    {
+        public float Latitude;
+        public float Longtitude;
+
+        public GeoCoord ToGeoCoord() => new(Latitude, Longtitude);
+    }
+
     public static Map Instance { get; private set; } // Singleton instance of the Map class
 
     [SerializeField] private float scale = 100f; // meter/unit
     [SerializeField] private Vector2 size; // Size of the map in units
+    [SerializeField] private GeoRectInspector boundingRectangle; // Inspector field for GeoRect
 
     public float Scale => scale; // Read-only property to access the scale
-    public Vector2 Size => size; // Read-only property to access the size
+    public Vector2 Size => new(boundingRectangle.Width / scale, boundingRectangle.Height / scale); // Read-only property to access the size
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
