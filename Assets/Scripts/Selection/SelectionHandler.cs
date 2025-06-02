06f7e7964e43d4dc2d0669f2ea71d2b73e82af9d
TomHuizing <tom.huizing@protonmail.com>
2025-06-02 17:42:38 +0200

    Worked on SelectionHandler

--- Assets/Scripts/Selection/SelectionHandler.cs
+++ Assets/Scripts/Selection/SelectionHandler.cs
@@ -9,17 +9,14 @@ public class SelectionHandler : MonoBehaviour
 {
     private Vector2 mouseStartScreen;
     private Vector2 mouseStartWorld;
+    private Vector2 mouseWorld;
+    private Vector2 mouseScreen;
     private bool isDragging = false;
-    private bool mouseDown = false;
+    private bool isPrimaryDown = false;
 
     [SerializeField] SelectionManager selectionManager;
     [SerializeField] private float deadZoneScreen;
     [SerializeField] private float deadZoneWorld;
-
-    void Update()
-    {
-        if(mouseDown)
-    }
 
     public void Select(InputAction.CallbackContext context)
     {
@@ -28,7 +25,68 @@ public class SelectionHandler : MonoBehaviour
         print("Click");
         if (EventSystem.current.IsPointerOverGameObject())
             return;
-        selectionManager.Select(GetSelectableFromHits(Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()), Vector2.zero, Mathf.Infinity)));
+        
+    }
+
+    public void MouseMove(InputAction.CallbackContext context)
+    {
+        mouseScreen = context.ReadValue<Vector2>();
+        mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
+        if (isPrimaryDown && !isDragging)
+        {
+            if (Vector2.Distance(mouseScreen, mouseStartScreen) > deadZoneScreen || Vector2.Distance(mouseWorld, mouseStartWorld) > deadZoneWorld)
+                isDragging = true;
+        }
+    }
+
+    public void PrimaryMouseButton(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                PrimaryMouseDown();
+                break;
+            case InputActionPhase.Canceled:
+                PrimaryMouseUp();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void PrimaryMouseDown()
+    {
+        isPrimaryDown = true;
+        mouseStartScreen = mouseScreen;
+        mouseStartWorld = mouseWorld;
+    }
+
+    private void PrimaryMouseUp()
+    {
+        isPrimaryDown = false;
+        if (isDragging)
+            EndDrag();
+        else
+            ClickSelect();
+    }
+
+    private void ClickSelect()
+    {
+        selectionManager.Select(GetSelectableFromHits(Physics2D.RaycastAll(mouseWorld, Vector2.zero, Mathf.Infinity)));
+    }
+
+    private void StartDrag()
+    {
+
+    }
+
+    private void EndDrag()
+    {
+        isDragging = false;
+        Vector2 min = Vector2.Min(mouseStartWorld, mouseWorld);
+        Vector2 size = Vector2.Max(mouseStartWorld, mouseWorld) - min;
+        Rect selectionRect = new(min, size);
+        selectionManager.Select(GetSelectablesInRect(selectionRect));
     }
 
     private Selectable GetSelectableFromHits(RaycastHit2D[] hits)
